547a454a74bfb7aa8746140d1bd8d23c0040883e
Julia K <33374959+jkkawano@users.noreply.github.com>
2018-07-28 08:23:06 -1000

    FE-2 Adding Webpack & Babel (#2)
    * add dockerized build
    
    * starting adding webpack & babel

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
